5e4a60f042bb758e5c358e11e6ac063c0e5c667c
Arnav Goel <arnavgoel182@gmail.com>
2023-11-19 01:55:43 +0800

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Act1 from './Activities/Act1';
 import Act2 from './Activities/Act2';
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path='/1' element={<Act1/>}></Route>
-      <Route path='/2' element={<Act2/>}></Route>
-    </Routes>
-  </Router>
+      <Routes>
+      <Route path="/" element={<Navigate to="/1" replace />} />
+        <Route path="/1" element={<Act1/>} />
+        <Route path="/2" element={<Act2/>} />
+      </Routes>
+    </Router>
   )
 }
 
